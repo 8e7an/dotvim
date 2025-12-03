ba335a5353bc49dfad909908f8812e3ced0b8594
Bevan Sharp <Bevan.Sharp@aff.gov.au>
2025-12-03 17:16:41 +1100

    Ignore pack/8e7an and updates to make_plugin_folders.sh script

--- scripts/make_plugin_folders.sh
+++ scripts/make_plugin_folders.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 echo -e "Creating plugin directories:\nplugin\nautoload\ndoc"
-mkdir -p ./pack/$1/start/$2/plugin
-mkdir -p ./pack/$1/start/$2/autoload
-mkdir -p ./pack/$1/start/$2/doc
+mkdir -p ~/.vim/pack/$1/start/$2/plugin
+mkdir -p ~/.vim/pack/$1/start/$2/autoload
+mkdir -p ~/.vim/pack/$1/start/$2/doc
 
+touch ~/.vim/pack/$1/start/$2/plugin/$2.vim
+# TODO: ask if the plugin/$2.vim file is to be created and if so create it as follows:
+touch ~/.vim/pack/$1/start/$2/plugin/$2.vim
+# TODO: ask if the doc/$2.txt file is to be created and if so create it as follows:
+touch ~/.vim/pack/$1/start/$2/doc/$2.txt
